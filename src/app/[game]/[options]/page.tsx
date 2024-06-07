d40be35128ab7fc127ba11d3b5e5dfd958618df7
Mirjana Krneta Vodenik <mirjana.krnetavodenik@tui.com>
2024-06-07 13:14:12 +0200

    chore: OptionsModal styling

--- src/app/[game]/[options]/page.tsx
+++ src/app/[game]/[options]/page.tsx
@@ -1,41 +1,43 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import OptionsModal from 'app/components/OptionsModal';
-import { useNavigate } from 'react-router-dom';
+import { OptionsModal } from 'app/components/OptionsModal';
+
 import { CloseButton } from 'app/components/buttons/close-button';
 import { Button } from 'app/components/buttons/button';
 import { useGameNavigation } from 'app/[game]/hooks/routes/use-game-navigation';
+import ReactModal from 'react-modal';
 
 export const OptionsPage: React.FC = () => {
-  const navigate = useNavigate();
-  const resourcesPath = useGameNavigation();
   const [showModal, setShowModal] = useState(false);
   const handleClose = () => {
     setShowModal(false);
-    navigate(resourcesPath.exitPath);
+  };
+  const handleOpen = () => {
+    setShowModal(true);
   };
   return (
     <>
       <nav className="position:relative position:absolute text-center text-2xl font-bold mt-24">
         <Link to="/options">options page</Link>
         <div>
-          <Button
-            onClick={() => setShowModal(true)}
-            className="h-[40px] backgroundColor:#85664a color:#f2e1d0"
-          >
+          <Button onClick={handleOpen}>
             {showModal ? (
-              <OptionsModal>
-                <div>
-                  <h1>Options</h1>
-                  <p>Here are some options</p>
+              <OptionsModal
+                isOpen={showModal}
+                onClose={!showModal}
+              >
+                <div className="grid grid-cols-2">
+                  <h2 className="text-lg font-medium text-gray-900">Preferences</h2>
+                  <CloseButton onClick={handleClose} />
+                </div>
+                <div className="mt-2 text-sm text-gray-500 grid grid-cols-3 grid-rows-1">
                   <div className="ul">tweeter</div>
                   <div className="ul">meta</div>
                   <div className="ul">dark/white theme</div>
-                  <CloseButton onClick={handleClose}>close</CloseButton>
                 </div>
               </OptionsModal>
             ) : null}
-            Click me
+            Open modal
           </Button>
         </div>
       </nav>
--- src/app/components/OptionsModal.tsx
+++ src/app/components/OptionsModal.tsx
@@ -1,8 +1,16 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const OptionsModal = ({ children }: { children: React.ReactNode }) => {
+type OptionsModalProps = {
+  isOpen?: boolean;
+  onClose?: boolean | undefined;
+  children?: React.ReactNode;
+};
+export const OptionsModal: React.FC<OptionsModalProps> = (props) => {
+  const { children } = props;
+
   const elRef = useRef<HTMLDivElement | null>(null);
+
   if (!elRef.current) {
     elRef.current = document.createElement('div');
   }
@@ -18,6 +26,13 @@ const OptionsModal = ({ children }: { children: React.ReactNode }) => {
       }
     };
   }, []);
-  return createPortal(<div>{children}</div>, elRef.current);
+  return createPortal(
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">{children}</div>
+        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse"></div>
+      </div>
+    </div>,
+    elRef.current
+  );
 };
-export default OptionsModal;
